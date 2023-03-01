48984dc6fcd0060d54b38b8fb5c6d80b3a33a1d0
amansaini-kiwi <aman.saini@kiwitech.com>
2023-03-01 14:15:44 +0530

    changes

--- account/views.py
+++ account/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from account.serializers import UserSerializer
+
+
+class ListCreateRetrieveUpdateDeleteApi(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
+                                        mixins.RetrieveModelMixin,
+                                        mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
