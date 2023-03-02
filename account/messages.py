9a5b63bc77a853f88e828ad1c2f38e1535ac1aaa
mohdasad-kiwi <mohd.asad@kiwitech.com>
2023-03-02 17:03:48 +0530

    Sign Up api With Login Api With Validations

---
+++ account/messages.py
@@ -0,0 +1,36 @@
+SIGNUP_VALIDATION_ERROR = {
+    'first_name': {
+        "blank": "first_name_BLANK",
+        "invalid": "first_name_INVALID",
+        "required": "first_name_REQUIRED",
+    },
+    'last_name': {
+        "blank": "last_name_BLANK",
+        "invalid": "last_name_INVALID",
+        "required": "last_name_REQUIRED",
+    },
+    'username': {
+        "blank": "username_BLANK",
+        "invalid": "username_INVALID",
+        "required": "username_REQUIRED",
+        "exits": "username_EXISTS"
+    },
+
+    'password': {
+        "blank": "PASSWORD_BLANK",
+        "invalid": "PASSWORD_INVALID",
+        "required": "PASSWORD_REQUIRED",
+        "not-match": "PASSWORD_fields didn't match",
+    },
+    'confirm_password': {
+        "blank": "PASSWORD_BLANK",
+        "invalid": "PASSWORD_INVALID",
+        "required": "PASSWORD_REQUIRED",
+    },
+    'email': {
+        "blank": "Email_BLANK",
+        "required": "Email_REQUIRED",
+        "exits": "email_EXISTS"
+    }
+
+}
